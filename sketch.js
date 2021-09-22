d3b41a6d258b1dd12a0dc26e724051e508c8e52a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-09-22 12:10:14 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,13 +7,17 @@ function setup() {
   g = new Ground(0,300, 600,100);
   g2 = new Ground(400,300,600,100);
   p = new Player(100, 100,50,50);
-}
+}//end Setup
 
 function draw() {
   background(0);
   p.display();
+  p.update();
   g.display();
   g2.display();
   g.update();
   g2.update();
-}
+ 
+  
+}//end draw
+
