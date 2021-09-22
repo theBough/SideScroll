74681a229bf9525ae8a99afc371fe23f97528244
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-09-22 12:09:49 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,19 +1,24 @@
 let imgPlayer;
-function Player(x,y,w,h){
+function Player(x, y, w, h) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.show = true;
   //this is the name of your ground picture.
-  imgPlayer =loadImage("mario2.gif");
-  this.display = function(){
-    if(this.show){
-      image(imgPlayer, this.x, this.y,this.w,this.h);
-    }//end if
-  }//end function
-  
-  this.update = function(){
-   
-  }//end function
-}//end Ground
+  imgPlayer = loadImage("mario2.gif");
+  this.display = function () {
+    if (this.show) {
+      image(imgPlayer, this.x, this.y, this.w, this.h);
+    } //end if
+  }; //end function
+
+  this.update = function () {
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += 5;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= 5;
+    }
+  }; //end function
+} //end Ground
