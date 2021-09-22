ffb1834d802174435a0a1aadb619a928b59f3cb9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-09-22 11:19:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let g;
-
+let g2;
+let p;
 
 function setup() {
   createCanvas(400, 400);
-  g = new Ground(0,300, 400,100);
-  
+  g = new Ground(0,300, 600,100);
+  g2 = new Ground(400,300,600,100);
+  p = new Player(100, 100,50,50);
 }
 
 function draw() {
-  background(50);
+  background(0);
+  p.display();
   g.display();
+  g2.display();
   g.update();
+  g2.update();
 }
