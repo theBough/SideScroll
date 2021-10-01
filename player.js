b0bb6f32b88bff952c4ef2b6e822045f2e088653
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-01 11:28:11 -0500

    Update player.js

--- player.js
+++ player.js
@@ -5,6 +5,9 @@ function Player(x, y, w, h) {
   this.w = w;
   this.h = h;
   this.show = true;
+  this.yVelocity = 0;
+  this.jumpStrength = 10;
+  this.gravity = 0.9;
   //this is the name of your ground picture.
   imgPlayer = loadImage("mario2.gif");
   this.display = function () {
@@ -14,11 +17,22 @@ function Player(x, y, w, h) {
   }; //end function
 
   this.update = function () {
+    this.yVelocity += this.gravity;
+    this.yVelocity *= 0.9;
+    this.y += this.yVelocity;
+    if(this.y + this.h > g.y){
+      this.y = g.y - this.h;
+      this.yVelocity = 0;
+    }//end if
+    
     if (keyIsDown(RIGHT_ARROW)) {
       this.x += 5;
     }
     if (keyIsDown(LEFT_ARROW)) {
       this.x -= 5;
     }
+    if(keyIsDown(32)){
+      this.yVelocity += -this.jumpStrength;
+    }//end if
   }; //end function
 } //end Ground
