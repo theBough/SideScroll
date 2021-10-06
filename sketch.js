efafdd52bedbf9b33e05af7cf460994946d5be5c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-06 11:35:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,33 @@
 let g;
 let g2;
 let p;
-let pf
+let pf = [];
+let whenToSend;
+let yRnd;
 
 function setup() {
   createCanvas(400, 400);
   g = new Ground(0,300, 600,100);
   g2 = new Ground(400,300,600,100);
   p = new Player(100, 250,50,50);
-  let yRnd = Math.floor(Math.random()* (height - 100));
-  pf = new Platform(width, yRnd,30,10);
+  yRnd = Math.floor(Math.random()* (height - 100));
+  pf.push(new Platform(width, yRnd,30,10));
+  whenToSend = 50;
 }//end Setup
 
 function draw() {
   background(0);
-  pf.display();
-  pf.update();
+  whenToSend --;
+  if(whenToSend < 0){
+    yRnd = Math.floor(Math.random()* (height - 100));
+    pf.push(new Platform(width, yRnd,30,10));
+    whenToSend = 50;
+  }
+  for(var i = 0 ; i < pf.length ; i++){
+    pf[i].display();
+    pf[i].update();
+  }
+  
   p.display();
   p.update();
   g.display();   
